7c8b29f099bc8578523945c44a20701a7070cdee
Daniel Molina <dmolina@decsai.ugr.es>
2019-04-13 16:32:37 +0200

    Refactor main.

--- matrix/main.cpp
+++ matrix/main.cpp
@@ -1,5 +1,5 @@
-#include <SFML/Graphics.hpp>
 #include "board.h"
+#include <SFML/Graphics.hpp>
 
 using namespace sf;
 
@@ -7,28 +7,26 @@ int main(void) {
   sf::RenderWindow window(sf::VideoMode(800, 800), "Tablero!");
   Board board(8, 8, 100);
 
-  while (window.isOpen())
-    {
-      sf::Event event;
-      while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed) {
-          window.close();
-        }
-
-        if (event.type == sf::Event::KeyPressed) {
-          if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
-            window.close();
-          }
-        }
-        else if (event.type == sf::Event::MouseButtonReleased) {
-          board.action(event.mouseButton.x, event.mouseButton.y);
-        }
+  while (window.isOpen()) {
+    sf::Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
       }
 
-      window.clear();
-      board.draw(window);
-      window.display();
+      if (event.type == sf::Event::KeyPressed) {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
+          window.close();
+        }
+      } else if (event.type == sf::Event::MouseButtonReleased) {
+        board.action(event.mouseButton.x, event.mouseButton.y);
+      }
     }
+
+    window.clear();
+    board.draw(window);
+    window.display();
+  }
 
   return 0;
 }
